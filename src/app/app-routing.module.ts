423664b969838279eabfa74298ca7d7cafbda36a
Lilith <lilithfurlan@gmail.com>
2021-10-09 17:39:53 -0300

    Contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  }, 
+  {
+    path:"contact",
+    component: ContactComponent,
+  },
+  {
+    path:"about",
+    component: AboutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
